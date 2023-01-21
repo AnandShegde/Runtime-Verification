83c4dc4c445873df6e3c2802b67f3f5514a89ab5
Arvind Kumar M <200020008@iitdh.ac.in>
2023-01-21 13:04:05 +0530

    Reading from monitor template and writing in monitor.cpp

--- Ass 1/src/main.cpp
+++ Ass 1/src/main.cpp
@@ -7,83 +7,6 @@
 
 
 
-
-std::string get_monitorString(std::string expression){
-    std::string monitor_template_start= " \
-    #include<iostream> \
-    #include <fstream> \
-    #include<unordered_map> \
-                            \
-    //getting index for the propostion  \
-    void get_index(std::unordered_map<int, std::string> &umap, std::string line, std::unordered_map<std::string, bool>&propVal){    \
-        int i = 0, index = 0;           \
-        std::string temp;               \
-        line += \",\";                  \
-        while(i < line.length()){       \
-            if(line[i] == ','){         \
-                umap[index++] = temp;   \
-                propVal[temp] = false;  \
-                temp = \"\";            \
-            }                           \
-            else{                       \
-                temp += line[i];        \
-            }                           \
-            i++;                        \
-        }                               \
-    }                                   \
-                                        \
-    //updating the boolean value of the proposition using map   \
-    void update_val(std::unordered_map<int, std::string> &umap, std::string line, std::unordered_map<std::string, bool>&propVal){   \
-        int i = 0, index = 0, val;                              \
-        line += \",\";                                          \
-        while(i < line.length()){                               \
-            if(line[i] == \',\'){                               \                          
-                index++;                                        \
-            }                                                   \
-            else{                                               \  
-                propVal[umap[index]] = line[i] - '0';           \
-            }                                                   \
-            i++;                                                \
-        }                                                       \
-    }                                                           \
-                                                                \
-                                                                \
-    int main(int argc, char* argv[]){                           \
-                                                                \
-                                                                \
-        if(argc != 3){                                          \
-            std::cout<<\"the format should be\\n monitor.cpp <input_file> <output_file>\";  \
-        }                                                       \
-                                                                \
-        int output;                                             \
-        std::string line, outputString;                         \
-        std::ifstream inputFile(argv[1]);                       \
-        std::ofstream outputFile(argv[2]);                      \
-        std::unordered_map<int, std::string> u_map;             \
-        std::unordered_map<std::string, bool>prop_val;          \
-                                                                \
-        getline(inputFile,line);                                \
-        get_index(u_map, line, prop_val);                       \
-                                                                \
-        while(getline(inputFile, line)){                        \
-            update_val(u_map, line, prop_val);                  \
-            output =";
-
-    std::string monitor_template_end = "\
-        ; // Replace with the expression got from the evaluate parse tree   \
-        outputString = output ? \"1\\n\" : \"0\\n\";                        \
-        outputFile << outputString;                                         \
-    }                                                                       \
-    inputFile.close();                                                      \
-    outputFile.close();                                                     \
-    return 0;                                                               \
-    }                                                                       \
-    ";
-
-    return monitor_template_start + expression + monitor_template_end;
-}
-
-
 std::string get_property(std::string fileName){
 
     std::string property;
@@ -97,10 +20,20 @@ std::string get_property(std::string fileName){
     return property;
 }
 
-void create_monitor_file(std::string monitorString){
+void create_monitor_file(std::string expression){
     std::ofstream monitorOutput("monitor.cpp");
+    std::ifstream monitorTempate("monitor_template.cpp");
+    std::string line, monitorString;
+    while(getline(monitorTempate,line)){
+        monitorString += line + "\n";
+    }
+    monitorTempate.close();
+
+    monitorString += "bool evaluate(std::unordered_map<std::string, bool>&prop_val){\n\treturn " + expression + ";\n}";
     monitorOutput << monitorString;
     monitorOutput.close();
+    
+    
 }
 
 
@@ -126,11 +59,8 @@ int main(int argc, char *argv[]){
     //std::cout<<expression<<std::endl;
 
 
-    //getting monitor.cpp template
-    monitorString = get_monitorString(expression);
-
     //writing monitor.cpp file
-    create_monitor_file(monitorString);
+    create_monitor_file(expression);
 
     return 0;
 }
