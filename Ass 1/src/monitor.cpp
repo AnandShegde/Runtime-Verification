f8d2b83d08ab2077cc2ff7b9d8ee2ab8b4457191
Arvind Kumar M <200020008@iitdh.ac.in>
2023-01-21 11:21:07 +0530

    Completed monitor.cpp

--- Ass 1/src/monitor.cpp
+++ Ass 1/src/monitor.cpp
@@ -1,5 +1,38 @@
 #include<iostream>
 #include <fstream>
+#include<unordered_map>
+
+void get_index(std::unordered_map<int, std::string> &umap, std::string line, std::unordered_map<std::string, bool>&propVal){
+    int i = 0, index = 0;
+    std::string temp;
+    line += ",";
+    while(i < line.length()){
+        if(line[i] == ','){
+            umap[index] = temp;
+            propVal[temp] = false;
+            index++;
+            temp = "";
+        }
+        else{
+            temp += line[i];
+        }
+        i++;
+    }
+}
+
+void update_val(std::unordered_map<int, std::string> &umap, std::string line, std::unordered_map<std::string, bool>&propVal){
+    int i = 0, index = 0, val;
+    line += ",";
+    while(i < line.length()){
+        if(line[i] == ','){
+            index++;
+        }
+        else{
+            propVal[umap[index]] = line[i] - '0';
+        }
+        i++;
+    }
+}
 
 int main(int argc, char* argv[]){
 
@@ -8,18 +41,21 @@ int main(int argc, char* argv[]){
         std::cout<<"the format should be\n monitor.cpp <input_file> <output_file>";
     }
 
+    int output;
     std::string line;
     std::ifstream inputFile(argv[1]);
+    std::unordered_map<int, std::string> u_map;
+    std::unordered_map<std::string, bool>prop_val;
+
     getline(inputFile,line);
-
+    get_index(u_map, line, prop_val);
+    
     while(getline(inputFile, line)){
-       
+        update_val(u_map, line, prop_val);
+        std::cout<<output<<std::endl;
     }
-
-
+    inputFile.close();
     
-    
-    
-
+    return 0;
 
 }
