6e5d4131ef3766afe97264ee928e519469cdef83
Arvind Kumar M <200020008@iitdh.ac.in>
2023-01-24 22:47:32 +0530

    Added script files for testing

---
+++ Ass 1/finddiff.sh
@@ -0,0 +1,9 @@
+diff outputs/property1/system1.verdict testcases/property1/system1.verdict > diff/property1/diff1.txt
+diff outputs/property1/system2.verdict testcases/property1/system2.verdict > diff/property1/diff2.txt
+diff outputs/property1/system3.verdict testcases/property1/system3.verdict > diff/property1/diff3.txt
+diff outputs/property1/system4.verdict testcases/property1/system4.verdict > diff/property1/diff4.txt
+
+diff outputs/property2/system1.verdict testcases/property2/system1.verdict > diff/property2/diff1.txt
+diff outputs/property2/system2.verdict testcases/property2/system2.verdict > diff/property2/diff2.txt
+diff outputs/property2/system3.verdict testcases/property2/system3.verdict > diff/property2/diff3.txt
+diff outputs/property2/system4.verdict testcases/property2/system4.verdict > diff/property2/diff4.txt
---
+++ Ass 1/testall.sh
@@ -0,0 +1,9 @@
+./perform_RV.sh testcases/property1/redgreen.property testcases/property1/system1.input outputs/property1/system1.verdict
+./perform_RV.sh testcases/property1/redgreen.property testcases/property1/system2.input outputs/property1/system2.verdict
+./perform_RV.sh testcases/property1/redgreen.property testcases/property1/system3.input outputs/property1/system3.verdict
+./perform_RV.sh testcases/property1/redgreen.property testcases/property1/system4.input outputs/property1/system4.verdict
+
+./perform_RV.sh testcases/property2/AP8.property testcases/property2/system1.input outputs/property2/system1.verdict
+./perform_RV.sh testcases/property2/AP8.property testcases/property2/system2.input outputs/property2/system2.verdict
+./perform_RV.sh testcases/property2/AP8.property testcases/property2/system3.input outputs/property2/system3.verdict
+./perform_RV.sh testcases/property2/AP8.property testcases/property2/system4.input outputs/property2/system4.verdict
